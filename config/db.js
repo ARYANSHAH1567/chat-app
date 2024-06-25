b7d19ef6a043c14942bc8a6ccb409d1450913d6b
Aryan Shah <aryanashah11@gmail.com>
2024-06-25 23:30:08 +0530

    fixing deployment error

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds for server selection
+      socketTimeoutMS: 45000, // Timeout after 45 seconds for socket operations
+      bufferTimeoutMS: 60000, 
     });
     console.log('MongoDB Connected');
   } catch (error) {
